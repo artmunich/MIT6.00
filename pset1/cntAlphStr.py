ebf3676088b554f72d8fe5bf1953fa9cfa6a8877
artmunich <huaixw@gmail.com>
2015-06-16 21:41:31 +0800

    Modify program for printing longest substring in alphabetical order

--- pset1/cntAlphStr.py
+++ pset1/cntAlphStr.py
@@ -1,12 +1,24 @@
-#list all alphabetical ordered substrings
-s='abzghicbat'
+#Print the longest substring of s in which the letters occur in alphabetical order
+#s='abcdefghijklmnopqrstuvwxyz'
+s='dhnbvsrghra'
 
 def strminus(s,substr):
     return s.replace(substr,'')
 
-
+maxlen=0
 for i in range(len(s)-1):
-    if s[i]>s[i+1]:
-        substr=s[:i+1]
-        print substr
-        s=strminus(s,substr)
+    for j in range(i,len(s)-1):
+        if s[j]>s[j+1]:
+            substr=s[i:j+1]
+            print ('substr='+substr)
+            if len(substr)>maxlen:
+                maxlen=len(substr)
+                maxstr=substr
+            break
+        elif (j==len(s)-2):
+            substr=s[i:j+2]
+            if len(substr)>maxlen:
+                maxlen=len(substr)
+                maxstr=substr
+            break
+print 'Longest substring in alphabetical order is: '+maxstr
