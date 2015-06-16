643ee8b3e73cd43ce25b9afe34fb6d4476b71379
artmunich <huaixw@gmail.com>
2015-06-16 21:55:50 +0800

    A better way to print longest substring

---
+++ pset1/cntLongestAlphStr_better.py
@@ -0,0 +1,16 @@
+#s = "azcbobobegghakl"
+#s='abcdefghijklmnopqrstuvwxyz'
+s='dhnbvsrghrs'
+start = 0
+end = 0
+temp_start = 0
+
+for i in range(1, len(s)):
+    if s[i-1] > s[i]:
+        temp_start = i
+        
+    if i - temp_start > end - start:
+        start = temp_start
+        end = i
+                                            
+print 'Longest substring in alphabetical order is:', s[start:end+1]
